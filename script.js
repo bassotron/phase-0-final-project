0a4a8c251882d9e41c16edcf8b9eba38e446c2bd
Will Lovell <willard.lovell@icloud.com>
2021-11-07 14:34:31 -0700

    added iframe for about page containing resume

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ h2.textContent = "";
 document.querySelector("body").appendChild(h2);
 
 
+
+window.addEventListener('message', function(e) {
+    var $iframe = jQuery("#myIframe");
+    var eventName = e.data[0];
+    var data = e.data[1];
+    switch(eventName) {
+      case 'setHeight':
+        $iframe.height(data);
+        break;
+    }
+  }, false);
+
+  function resize() {
+    var height = document.getElementsByTagName("html")[0].scrollHeight;
+    window.parent.postMessage(["setHeight", height], "*"); 
+  }
